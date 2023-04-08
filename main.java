5b1b130747822b08feebed5661357e6d177ebeda
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-04-08 13:25:10 +0800

    initial

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main{
+        public static void main(String[] args){
+                Scanner input = new Scanner(System.in);
+                String binInput = input.next(); // binary input
+                String base2Input = input.next(); // base-2 input
+                // add input for GRS or rounding
+                int digitsSupported = input.nextInt(); // number of digits supported
+        }
+}
+
