74d2ef5f3c40dfa9a8e868d0270def5c867ac510
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-04-15 19:57:07 +0800

    deleting unused files

--- Operand.java
+++
@@ -1,9 +0,0 @@
-public class Operand {
-    String binary = "";
-    int exponent = 0;
-
-    public Operand(String binary, int exponent){
-        this.binary = binary;
-        this.exponent = exponent;
-    }
-}
--- out/production/CSARCH2-IEEEWebAp/README.md
+++
@@ -1,2 +0,0 @@
-# CSARCH2-IEEEWebAp
-A webapp for IEEE-754 Binary-32 floating-point operation
