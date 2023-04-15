140b6e92d7d95e86e57c9a29e1a42bfdbdb71be7
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2023-04-15 08:30:24 +0800

    Fixed formatting

--- _test.java
+++ _test.java
@@ -1,34 +1,47 @@
 public class _test {
     public static void main(String[] args){
-        Operand first = new Operand("110111.11011111", 10);
-        Operand second = new Operand("111111.0100101", 8);
+        Operand first = new Operand("1.00111101", 5);
+        Operand second = new Operand("1.00111101", 3);
+
+
+        // Print Given
+        System.out.println("Given:");
+        System.out.println("  " + first.binary + " x2^" + first.exponent);
+        System.out.println("  " + second.binary + " x2^" + second.exponent);
+        System.out.println("-------------");
+
+
+        // Normalize
         Normalize firstNormalize = new Normalize(first);
         Normalize secondNormalize = new Normalize(second);
         firstNormalize.normalize();
         secondNormalize.normalize();
         String operandOne = firstNormalize.getBinInput();    int exponentOne = firstNormalize.getBinExponent();
         String operandTwo = secondNormalize.getBinInput();   int exponentTwo = secondNormalize.getBinExponent();
-        boolean grs = false;            int digits = 4;
+        boolean grs = true;            int digits = 5;
 
         /* Use @Samuel Uy's Normalization here if operands are crazy like 1000.1111 and not in 1.f */
-
         InitialNormalize initialNormalize = new InitialNormalize(operandOne, exponentOne, operandTwo, exponentTwo, grs, digits);
         initialNormalize.performShift();
         initialNormalize.performRound();
         initialNormalize.performNegative();
 
+        System.out.println("Initial normalization");
         System.out.println("  " + initialNormalize.getFirstOperand() + " x2^" + initialNormalize.getExponent());
-        System.out.println(" " + initialNormalize.getSecondOperand() + " x2^" + initialNormalize.getExponent());
+        System.out.println("  " + initialNormalize.getSecondOperand() + " x2^" + initialNormalize.getExponent());
 
         Operation operation = new Operation(initialNormalize.getFirstOperand(), initialNormalize.getSecondOperand(), grs, digits);
         operation.performAddition();
         System.out.println("-------------");
+        System.out.println("Operation sum:");
         System.out.println(" " + operation.getSum() + " x2^" + initialNormalize.getExponent());
 
         PostNormalize postNormalize = new PostNormalize(operation.getSum(), initialNormalize.getExponent(), digits);
         postNormalize.performShift();
         postNormalize.performRound();
         System.out.println("-------------");
+        System.out.println("Post operation normalization (final answer)");
+
         System.out.println("  " + postNormalize.getSum() + " x2^" + postNormalize.getExponent());
 
         /* Use @Samuel Uy's Normalization here if sum is 0.0001 and not in 10.f */
